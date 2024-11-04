5a1a2628b34b72e94e3e3675e908a772bb841aaa
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-11-04 17:38:58 +0900

    Implemeted a sysfs file sysfs:my_ops/ctl

--- my_ops.c
+++ my_ops.c
@@ -8,15 +8,63 @@ MODULE_DESCRIPTION("A simple implementation of a kernel module using struct_ops"
 MODULE_LICENSE("GPL v2");
 MODULE_VERSION("0.1");
 
+// =============================================================================
+// The following implementations are for files in sysfs:my_ops.
+// =============================================================================
+static int n = 0;
+
+static ssize_t ctl_show(struct kobject *kobj, struct kobj_attribute *attr,
+                        char *buf)
+{
+	return sprintf(buf, "ctl_show: n=%d\n", n);
+}
+
+static ssize_t ctl_store(struct kobject *kobj, struct kobj_attribute *attr,
+			 const char *buf, size_t count)
+{
+	int ret;
+
+	ret = kstrtoint(buf, 10, &n);
+	if (ret < 0) {
+		pr_err("failed to convert buf to int\n");
+		return ret;
+	}
+
+	pr_info("ctl_store: n=%d\n", n);
+	return count;
+}
+
+// sysfs:my_ops dir
+static struct kobject *my_ops_kobj;
+// sysfs:my_ops/ctl file
+static struct kobj_attribute ctl_attr = __ATTR(ctl, 0660, ctl_show, ctl_store);
+
 static int __init my_ops_init(void)
 {
+	int err;
+
 	pr_info("my_ops_init\n");
+
+	my_ops_kobj = kobject_create_and_add("my_ops", kernel_kobj);
+	if (!my_ops_kobj) {
+		pr_err("failed to create and add my_ops kobject\n");
+		return -ENOMEM;
+	}
+
+	err = sysfs_create_file(my_ops_kobj, &ctl_attr.attr);
+	if (err) {
+		pr_err("failed to create file sysfs:my_ops/ctl\n");
+		return err;
+	}
+
 	return 0;
 }
 
 static void __exit my_ops_exit(void)
 {
 	pr_info("my_ops_exit\n");
+
+	kobject_put(my_ops_kobj);
 }
 
 module_init(my_ops_init);
