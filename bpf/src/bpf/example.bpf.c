b426f5ec5861dd1b070bf1bded64c68b7321c39e
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-11-24 23:52:57 +0900

    Implemnt a simple global counter in BPF program

--- bpf/src/bpf/example.bpf.c
+++ bpf/src/bpf/example.bpf.c
@@ -3,12 +3,18 @@
 #include <bpf/bpf_tracing.h>
 char LICENSE[] SEC("license") = "GPL";
 
+u64 cnt = 0;
+
 SEC("struct_ops/my_ops_calculate")
 u64 BPF_PROG(my_ops_calculate, u64 n)
 {
 	u64 retval;
 
 	retval = n * n;
+
+	cnt += 1;
+	retval += cnt;
+	
 	bpf_printk("calculate(%d) -> %d", n, retval);
 	return retval;
 }
